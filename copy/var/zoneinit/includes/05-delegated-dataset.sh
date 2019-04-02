b327a719cb7fc804acae0f5251c86f82a40fbe7d
Thomas Merkel <tm@core.io>
2019-04-02 14:05:50 +0200

    Install rabbitmq database and keep it in delegate dataset

--- copy/var/zoneinit/includes/05-delegated-dataset.sh
+++ copy/var/zoneinit/includes/05-delegated-dataset.sh
@@ -3,10 +3,15 @@ UUID=$(mdata-get sdc:uuid)
 DDS=zones/${UUID}/data
 
 if zfs list ${DDS} 1>/dev/null 2>&1; then
-	zfs create ${DDS}/pgsql || true
+	zfs create ${DDS}/pgsql    || true
+	zfs create ${DDS}/rabbitmq || true
 
 	if ! zfs get -o value -H mountpoint ${DDS}/pgsql | grep -q /var/pgsql; then
 		zfs set compression=lz4 ${DDS}/pgsql
 		zfs set mountpoint=/var/pgsql ${DDS}/pgsql
+	fi
+	if ! zfs get -o value -H mountpoint ${DDS}/rabbitmq | grep -q /var/db/rabbitmq; then
+		zfs set mountpoint=/var/db/rabbitmq ${DDS}/rabbitmq
+		install -d -u rabbitmq -g rabbitmq -m 770 /var/db/rabbitmq
 	fi
 fi
